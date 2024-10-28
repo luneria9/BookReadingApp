e6bc41d622304d1b3c258dc165786c11315d9480
Yun Chen Qian <yun.qian@dawsoncollege.qc.ca>
2024-10-28 11:13:49 -0400

    chore: add starter code for navigation

--- BookReadingApp/app/src/main/java/com/example/bookreadingapp/MainActivity.kt
+++ BookReadingApp/app/src/main/java/com/example/bookreadingapp/MainActivity.kt
@@ -1,28 +1,57 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.bookreadingapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.material3.*
+import androidx.compose.runtime.getValue
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass.Companion.Compact
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass.Companion.Expanded
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass.Companion.Medium
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.NavHostController
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.bookreadingapp.ui.NavBarItems
+import com.example.bookreadingapp.ui.NavRoutes.*
+import com.example.bookreadingapp.ui.screens.*
+import com.example.bookreadingapp.ui.utils.AdaptiveNavigationType
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass as calculateWindowSizeClass1
+import androidx.compose.material3.Scaffold
 import com.example.bookreadingapp.ui.theme.BookReadingAppTheme
 
+
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BookReadingAppTheme {
+            BookReadingAppTheme  {
+                val windowSize = calculateWindowSizeClass1(this)
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
+                    AdaptiveNavigationApp(
+                        windowSizeClass = windowSize.widthSizeClass,
+                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -31,17 +60,154 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
+fun NavigationHost(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = Home.route
+    ) {
+        composable(Home.route) {
+            HomeScreen()
+        }
+
+        composable(Library.route) {
+            LibraryScreen()
+        }
+
+        composable(Search.route) {
+            SearchScreen()
+        }
+
+        composable(Contents.route) {
+            ContentsScreen()
+        }
+
+        composable(Reading.route) {
+            ReadingScreen()
+        }
+    }
+}
+
+@Composable
+@ExperimentalMaterial3Api
+fun AdaptiveNavigationApp(windowSizeClass: WindowWidthSizeClass, modifier: Modifier) {
+    val navController = rememberNavController()
+
+     val adaptiveNavigationType = when (windowSizeClass) {
+        Compact -> AdaptiveNavigationType.BOTTOM_NAVIGATION
+        Medium -> AdaptiveNavigationType.NAVIGATION_RAIL
+        else -> AdaptiveNavigationType.PERMANENT_NAVIGATION_DRAWER
+    }
+
+    Scaffold(
+        topBar = { TopAppBar(title = { Text("Adaptive Navigation illustration app") }) },
+        bottomBar = {
+            if (adaptiveNavigationType == AdaptiveNavigationType.BOTTOM_NAVIGATION) {
+                BottomNavigationBar(navController = navController)
+            }
+        }
+    ) { paddingValues ->
+        Row(modifier = Modifier.padding(paddingValues)) {
+            if (adaptiveNavigationType == AdaptiveNavigationType.PERMANENT_NAVIGATION_DRAWER) {
+                PermanentNavigationDrawerComponent()
+            }
+            if (adaptiveNavigationType == AdaptiveNavigationType.NAVIGATION_RAIL) {
+                NavigationRailComponent(navController = navController)
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+            ) {
+                NavigationHost(navController = navController)
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    NavigationBar {
+        val backStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoutes = backStackEntry?.destination?.route
+        NavBarItems.BarItems.forEach { navItem ->
+            NavigationBarItem(
+                selected = currentRoutes == navItem.route,
+                onClick = {
+                    navController.navigate(navItem.route)
+                },
+                icon = {
+                    Icon(
+                        imageVector = navItem.image,
+                        contentDescription = navItem.title
+                    )
+                },
+                label = {
+                    Text(text = navItem.title)
+                },
+            )
+        }
+    }
+}
+
+@Composable
+fun NavigationRailComponent(navController: NavController) {
+    NavigationRail {
+        val backStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoutes = backStackEntry?.destination?.route
+        NavBarItems.BarItems.forEach { navItem ->
+            NavigationRailItem(
+                selected = currentRoutes == navItem.route,
+                onClick = { navController.navigate(navItem.route) },
+                icon = {
+                    Icon(navItem.image, contentDescription = navItem.title)
+                },
+                label = {
+                    Text(text = navItem.title)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun PermanentNavigationDrawerComponent() {
+     val navController = rememberNavController()
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoutes = backStackEntry?.destination?.route
+    PermanentNavigationDrawer(
+        drawerContent = {
+            PermanentDrawerSheet {
+                Column {
+                    Spacer(Modifier.height(12.dp))
+                    NavBarItems.BarItems.forEach { navItem ->
+                        NavigationDrawerItem(
+                            selected = currentRoutes == navItem.route,
+                            onClick = {
+                                    navController.navigate(navItem.route)
+                            },
+                            icon = {
+                                Icon(navItem.image, contentDescription = navItem.title)
+                            },
+                            label = { Text(text = navItem.title) }
+                        )
+                    }
+                }
+            } },
+        content = {
+             Box(modifier = Modifier.fillMaxSize()) {
+                 //The call to NavigationHost is necessary to display the screen based on the route
+                NavigationHost(navController = navController)
+            }
+        }
     )
 }
 
-@Preview(showBackground = true)
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true, widthDp = 1000)
 @Composable
 fun GreetingPreview() {
     BookReadingAppTheme {
-        Greeting("Android")
+        AdaptiveNavigationApp(windowSizeClass = Expanded, modifier = Modifier)
     }
 }
--- BookReadingApp/app/src/main/java/com/example/bookreadingapp/ui/theme/Theme.kt
+++ BookReadingApp/app/src/main/java/com/example/bookreadingapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.bookreadingapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,17 +11,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
 
-        /* Other default colors to override
+    /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
@@ -35,10 +34,10 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun BookReadingAppTheme(
-        darkTheme: Boolean = isSystemInDarkTheme(),
-        // Dynamic color is available on Android 12+
-        dynamicColor: Boolean = true,
-        content: @Composable () -> Unit
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -51,8 +50,8 @@ fun BookReadingAppTheme(
     }
 
     MaterialTheme(
-            colorScheme = colorScheme,
-            typography = Typography,
-            content = content
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
     )
 }
