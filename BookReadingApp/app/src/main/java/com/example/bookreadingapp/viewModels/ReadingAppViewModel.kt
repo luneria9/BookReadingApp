42c0f9e43ddc15160a91bb8aa2d152faed20ce72
Yun Chen Qian <yun.qian@dawsoncollege.qc.ca>
2024-10-30 10:19:53 -0400

    chore: setup viewModel class

---
+++ BookReadingApp/app/src/main/java/com/example/bookreadingapp/viewModels/ReadingAppViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.bookreadingapp.viewModels
+
+import androidx.compose.material3.DrawerValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class ReadingAppViewModel : ViewModel() {
+}
