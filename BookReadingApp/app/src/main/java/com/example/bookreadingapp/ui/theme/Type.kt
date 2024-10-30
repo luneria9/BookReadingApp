53702c9a6f09ad4775ab9b4d630840ac8a3b0321
luneria <luneria444@gmail.com>
2024-10-30 13:54:56 -0400

    Ädd and initialize six custom fonts

--- BookReadingApp/app/src/main/java/com/example/bookreadingapp/ui/theme/Type.kt
+++ BookReadingApp/app/src/main/java/com/example/bookreadingapp/ui/theme/Type.kt
@@ -5,6 +5,23 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import com.example.bookreadingapp.R
+
+val BonaNona = FontFamily(
+    Font(R.font.bonanova_regular),
+    Font(R.font.bonanova_bold, FontWeight.Bold)
+)
+
+val MerriWeather = FontFamily(
+    Font(R.font.merriweather_regular),
+    Font(R.font.merriweather_bold, FontWeight.Bold)
+)
+
+val NanumMyeongjo = FontFamily(
+    Font(R.font.nanummyeongjo_regular),
+    Font(R.font.nanummyeongjo_bold, FontWeight.Bold)
+)
 
 //referenced from https://gitlab.com/crdavis/adaptivenavigationegcode/-/tree/master?ref_type=heads
 // Set of Material typography styles to start with
