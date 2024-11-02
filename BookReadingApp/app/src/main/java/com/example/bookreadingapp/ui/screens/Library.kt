4751e964c7b25871f1c31f45711820fe52c495db
luneria <luneria444@gmail.com>
2024-11-02 17:49:01 -0400

    Add LibraryScreen with grid layout for book images

--- BookReadingApp/app/src/main/java/com/example/bookreadingapp/ui/screens/Library.kt
+++ BookReadingApp/app/src/main/java/com/example/bookreadingapp/ui/screens/Library.kt
@@ -1,30 +1,78 @@
 package com.example.bookreadingapp.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.bookreadingapp.R
+import com.example.bookreadingapp.ui.theme.BookReadingAppTheme
 
+// Referred to https://developer.android.com/codelabs/basic-android-kotlin-compose-material-theming#6
 @Composable
 fun LibraryScreen() {
+    val sampleBooks = listOf(
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground
+    )
+
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(dimensionResource(R.dimen.padding_medium)),
+        contentAlignment = Alignment.TopCenter
     ) {
-        Icon(
-            imageVector = Icons.Filled.ThumbUp,
-            contentDescription = "Library Screen",
-            tint = Color.Blue,
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            contentPadding = PaddingValues(dimensionResource(R.dimen.padding_medium))
+        ) {
+            items(sampleBooks) {
+                BookItem(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun BookItem(bookImg: Int) {
+    Card(
+        modifier = Modifier
+            .padding(dimensionResource(R.dimen.spacer_small))
+    ) {
+        Image(
+            painter = painterResource(bookImg),
+            contentDescription = null,
             modifier = Modifier
-                .size(150.dp)
-                .align(Alignment.Center)
+                .size(100.dp)
         )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PreviewLibraryScreen() {
+    BookReadingAppTheme {
+        LibraryScreen()
     }
 }
