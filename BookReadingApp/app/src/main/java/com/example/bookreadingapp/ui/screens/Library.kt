68b168a5fafb374bbb7e01e5380d8b8d1be64fb7
luneria <luneria444@gmail.com>
2024-11-03 12:23:20 -0500

    Remove unnecessary imports

--- BookReadingApp/app/src/main/java/com/example/bookreadingapp/ui/screens/Library.kt
+++ BookReadingApp/app/src/main/java/com/example/bookreadingapp/ui/screens/Library.kt
@@ -11,17 +11,11 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -67,7 +61,10 @@ fun LibraryScreen(navController: NavController) {
 
 // Composable function for individual book items
 @Composable
-fun BookItem(bookImg: Int, onClick: () -> Unit) {
+fun BookItem(
+    bookImg: Int,
+    onClick: () -> Unit
+) {
     Card(
         modifier = Modifier
             .padding(dimensionResource(R.dimen.spacer_small))
@@ -78,8 +75,7 @@ fun BookItem(bookImg: Int, onClick: () -> Unit) {
             contentDescription = null,
             modifier = Modifier
                 .size(100.dp)
-                .clip(RoundedCornerShape(dimensionResource(R.dimen.spacer_small))),
-            contentScale = ContentScale.Crop
+                .clip(RoundedCornerShape(dimensionResource(R.dimen.spacer_small)))
         )
     }
 }
