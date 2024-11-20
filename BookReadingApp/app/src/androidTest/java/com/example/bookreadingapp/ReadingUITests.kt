6cb7823a6a3d243015c363b5ba8f1c6e191fb57e
luneria <luneria444@gmail.com>
2024-11-20 17:34:13 -0500

    Fix the ReadingUITest

--- BookReadingApp/app/src/androidTest/java/com/example/bookreadingapp/ReadingUITests.kt
+++ BookReadingApp/app/src/androidTest/java/com/example/bookreadingapp/ReadingUITests.kt
@@ -1,5 +1,6 @@
 package com.example.bookreadingapp
 
+import android.content.Context
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsOff
@@ -11,19 +12,28 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bookreadingapp.ui.screens.ReadingScreen
 import com.example.bookreadingapp.ui.theme.BookReadingAppTheme
 import com.example.bookreadingapp.viewModels.ReadingAppViewModel
+import com.example.bookreadingapp.viewModels.ReadingAppViewModelFactory
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mockito.mock
 
 class ReadingUITests {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    private lateinit var context: Context
+    private lateinit var viewModel: ReadingAppViewModel
+
     @Before
     fun setup() {
+        context = mock(Context::class.java)
+
+        // Use the factory to get the viewModel
+        val viewModelFactory = ReadingAppViewModelFactory(context)
         composeTestRule.setContent {
             BookReadingAppTheme {
-                val viewModel: ReadingAppViewModel = viewModel()
+                viewModel = viewModel(factory = viewModelFactory)
 
                 ReadingScreen(
                     readingMode = viewModel.readingMode,
