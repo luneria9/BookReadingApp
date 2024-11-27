f1572cc5d61a045c3ee95e4fe879ab3403b9a203
Yun Chen Qian <yun.qian@dawsoncollege.qc.ca>
2024-11-26 23:35:20 -0500

    feat: implement room in viewModel

--- BookReadingApp/app/src/main/java/com/example/bookreadingapp/viewModels/ReadingAppViewModel.kt
+++ BookReadingApp/app/src/main/java/com/example/bookreadingapp/viewModels/ReadingAppViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.bookreadingapp.viewModels
 
+import android.app.Application
 import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -8,12 +9,23 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.bookreadingapp.data.ReadingRoomDatabase
+import com.example.bookreadingapp.data.entities.Books
+import com.example.bookreadingapp.data.entities.Chapters
+import com.example.bookreadingapp.data.entities.Images
+import com.example.bookreadingapp.data.entities.Pages
+import com.example.bookreadingapp.data.entities.SubChapters
+import com.example.bookreadingapp.data.repository.BooksRepository
+import com.example.bookreadingapp.data.repository.ChaptersRepository
+import com.example.bookreadingapp.data.repository.ImageRepository
+import com.example.bookreadingapp.data.repository.PageRepository
+import com.example.bookreadingapp.data.repository.SubChaptersRepository
 import com.example.bookreadingapp.fileSystem.FileSystem
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-class ReadingAppViewModel(private val fileSystem: FileSystem) : ViewModel() {
+class ReadingAppViewModel(private val fileSystem: FileSystem, application: Application) : ViewModel() {
     private val _directoryContents = MutableLiveData<List<String>>()
     val directoryContents: LiveData<List<String>> = _directoryContents
 
@@ -62,4 +74,113 @@ class ReadingAppViewModel(private val fileSystem: FileSystem) : ViewModel() {
         updateDirectoryContents(directoryName)
     }
 
+    val allBooks: LiveData<List<Books>>
+    private val booksRepository: BooksRepository
+    private val chaptersRepository: ChaptersRepository
+    private val subchaptersRepository: SubChaptersRepository
+    private val pagesRepository: PageRepository
+    private val imagesRepository: ImageRepository
+    val searchResultsBooks: MutableLiveData<List<Books>>
+    val searchResultsChapters: MutableLiveData<List<Chapters>>
+    val searchResultsSubChapters: MutableLiveData<List<SubChapters>>
+    val searchResultsPages: MutableLiveData<List<Pages>>
+    val searchResultsImages: MutableLiveData<List<Images>>
+
+    init {
+        val db = ReadingRoomDatabase.getInstance(application)
+        val booksDao = db.booksDao()
+        val chaptersDao = db.chaptersDao()
+        val subchaptersDao = db.subchaptersDao()
+        val pagesDao = db.pagesDao()
+        val imagesDao = db.imagesDao()
+        booksRepository = BooksRepository(booksDao)
+        chaptersRepository = ChaptersRepository(chaptersDao)
+        subchaptersRepository = SubChaptersRepository(subchaptersDao)
+        pagesRepository = PageRepository(pagesDao)
+        imagesRepository = ImageRepository(imagesDao)
+
+        allBooks = booksRepository.allBooks
+        searchResultsBooks = booksRepository.searchResults
+        searchResultsChapters = chaptersRepository.searchResults
+        searchResultsSubChapters = subchaptersRepository.searchResults
+        searchResultsPages = pagesRepository.searchResults
+        searchResultsImages = imagesRepository.searchResults
+    }
+
+    //books
+    fun insertBook(book: Books){
+        booksRepository.insertBook(book)
+    }
+
+    fun findBookId(id: Int){
+        booksRepository.findBookId(id)
+    }
+
+    fun findBookTitle(title: String) {
+        booksRepository.findBookTitle(title)
+    }
+
+    // chapters
+    fun insertChapter(chapters: Chapters){
+        chaptersRepository.insertChapter(chapters)
+    }
+
+    fun findChapterId(id: Int){
+        chaptersRepository.findChapterId(id)
+    }
+
+    fun findChapterTitle(title: String){
+        chaptersRepository.findChapterTitle(title)
+    }
+
+    fun findChaptersFromBook(id: Int){
+        chaptersRepository.findChaptersOfBook(id)
+    }
+
+    // subchapters
+    fun insertSubChapter(subChapters: SubChapters){
+        subchaptersRepository.insertSubChapter(subChapters)
+    }
+
+    fun findSubChapterId(id: Int){
+        subchaptersRepository.findSubChapterId(id)
+    }
+
+    fun findSubChapterTitle(title: String){
+        subchaptersRepository.findSubChapterTitle(title)
+    }
+
+    fun findSubChaptersOfChapter(id: Int){
+        subchaptersRepository.findSubChaptersOfChapter(id)
+    }
+
+    // pages
+    fun insertPage(pages: Pages){
+        pagesRepository.insertPage(pages)
+    }
+
+    fun findPageId(id: Int){
+        pagesRepository.findPageId(id)
+    }
+
+    fun findPageNumber(pageNumber: Int){
+        pagesRepository.findPageNumber(pageNumber)
+    }
+
+    fun findPageOfSubChapter(id: Int){
+        pagesRepository.findPagesOfSubchapter(id)
+    }
+
+    // images
+    fun insertImage(images: Images){
+        imagesRepository.insertImage(images)
+    }
+
+    fun findImageId(id: Int){
+        imagesRepository.findImageId(id)
+    }
+
+    fun findImagesOfPage(id: Int){
+        imagesRepository.findImagesOfPage(id)
+    }
 }
