ebbfe78196af89b9d91a31c550a3940c9221f789
Kristian Garkov <kristian.garkov04@gmail.com>
2024-12-05 00:00:32 -0500

    Fix unit tests

--- BookReadingApp/app/src/androidTest/java/com/example/bookreadingapp/SearchUITests.kt
+++ BookReadingApp/app/src/androidTest/java/com/example/bookreadingapp/SearchUITests.kt
@@ -1,9 +1,18 @@
 package com.example.bookreadingapp
 
+import android.app.Application
+import android.content.Context
+import android.preference.PreferenceManager
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import androidx.test.core.app.ApplicationProvider
 import com.example.bookreadingapp.ui.screens.SearchScreen
+import com.example.bookreadingapp.ui.theme.BookReadingAppTheme
+import com.example.bookreadingapp.viewModels.ReadingAppViewModel
+import com.example.bookreadingapp.viewModels.ReadingAppViewModelFactory
 import org.junit.Rule
 import org.junit.Test
 
@@ -12,11 +21,27 @@ internal class SearchUITests {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    private lateinit var context: Context
+    private lateinit var viewModel: ReadingAppViewModel
+    private lateinit var application: Application
+
     @OptIn(ExperimentalMaterial3Api::class)
     @Test
     fun validateSearchMessage() {
+        context = ApplicationProvider.getApplicationContext()
+        application = context.applicationContext as Application
+
+        // Use the factory to get the viewModel
+        val viewModelFactory = ReadingAppViewModelFactory(context, application)
+
         composeTestRule.setContent {
-            SearchScreen()
+            BookReadingAppTheme {
+                viewModel = viewModel(factory = viewModelFactory)
+                SearchScreen(
+                    viewModel = viewModel,
+                    navController = rememberNavController()
+                )
+            }
         }
 
         composeTestRule.onNodeWithText("Search for books").assertExists()
